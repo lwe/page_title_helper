500d2dfe2fadd460d278829df8ae1e5c001bf24f
lukas <lukas.westermann@gmail.com>
2009-07-07 15:28:32 +0200

    abstracted internal ActiveXyz-magic a bit away, so that changes to that part will be easier

--- lib/page_title_helper.rb
+++ lib/page_title_helper.rb
@@ -30,27 +30,60 @@ module PageTitleHelper
     Interpolations.send(:define_method, key, &block)
   end
   
+  # Default options, which are globally referenced and can
+  # be changed globally, which might be useful in some cases.
+  def self.options
+    @options ||= {
+      :format => ':app - :title',
+      :default => :'app.tagline'
+    }
+  end
+  
   def page_title(options = nil, &block)
     if block_given?
-      page_title = yield
-      content_for(:page_title) { page_title }
-      return page_title
+      content_for(:page_title) { yield }
+      return read_page_title_content_block
     end
     
-    options = options || {}
-    
+    options = PageTitleHelper.options.merge(options || {})
+    options.assert_valid_keys(:app, :key, :default, :format)
+    # just return the applications name
     return Interpolations.app('', {}) if options[:app] == true
     
-    page_title = instance_variable_get(:'@content_for_page_title') # NOTE: what happens if this changes...
-    page_title = I18n.translate(options[:key] || i18n_template_key('title'), :default => (options[:default] || :'app.tagline')) if page_title.blank?      
+    # read page title
+    page_title = read_page_title_content_block
+    page_title = I18n.translate(options[:key] || i18n_template_key('title'), :default => options[:default]) if page_title.blank?
+    
+    # return page title if format is set explicitly to false
     return page_title if options[:format] == false
     
+    # else -> interpolate
     Interpolations.interpolate options[:format] || ':app - :title', page_title, options
   end
   
   protected
+    
+    # Access <tt>@content_for_page_title</tt> variable, though this is a tad
+    # hacky, because... what if they (the rails guys) change the behaviour of
+    # <tt>content_for</tt>? Well, in Rails 2.3.x it works so far.
+    #
+    # But to simplify compatibility with later versions, this method kinda abstracts
+    # away access to the content within a <tt>content_for</tt> block.
+    def read_page_title_content_block
+      instance_variable_get(:'@content_for_page_title')
+    end
+    
+    # Access +ActionView+s internal <tt>@_first_render</tt> variable, to access
+    # template first rendered, this is to help create the DRY-I18n-titles magic,
+    # and also kind of a hack, because this really seems to be some sort if
+    # internal variable, that's why it's "abstracted" away as well.
+    #
+    def read_first_render_path
+      @_first_render.template_path
+    end
+    
     def i18n_template_key(append = nil)
-      ikey = @_first_render.template_path.gsub(/\.html\.erb$/, '').tr('/', '.')
+      ikey = read_first_render_path.gsub(/\.html\.erb$/, '').tr('/', '.')
       ikey = ikey + "." + append.to_s unless append.nil?      
       ikey
     end
