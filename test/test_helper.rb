827199c8886ef65eb997f331778ac70cf5a975b8
lukas <lukas.westermann@gmail.com>
2009-07-08 09:53:20 +0200

    include action_view to get 100% coverage and exclude test/unit for IRB sessions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
-require 'test/unit'
 require 'active_support'
-require 'active_support/test_case'
+require 'action_view'
+
+unless Object.const_defined?('IRB')
+  require 'test/unit'
+  require 'active_support/test_case'
+end
 
 ROOT = File.expand_path File.dirname(File.dirname(__FILE__))
 RAILS_ROOT = '/this/is/just/for/testing/page_title_helper'
